18e26a027c9d5d6a520d6b6440d16516171e3797
Gabriel Dechichi <gabrieldechichi@gmail.com>
2020-08-09 16:02:40 -0600

    Final Aula 06

---
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer2D.Character;
+
+[RequireComponent(typeof(CharacterMovement2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    CharacterMovement2D playerMovement;
+    SpriteRenderer spriteRenderer;
+    PlayerInput playerInput;
+
+    public Sprite crouchedSprite;
+    public Sprite idleSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<CharacterMovement2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimentacao
+        Vector2 movementInput = playerInput.GetMovementInput();
+        playerMovement.ProcessMovementInput(movementInput);
+
+        if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        //Pulo
+        if (playerInput.IsJumpButtonDown())
+        {
+            playerMovement.Jump();
+        }
+        if (playerInput.IsJumpButtonHeld() == false)
+        {
+            playerMovement.AbortJump();
+        }
+
+        //Agachar
+        if (playerInput.IsCrouchButtonDown())
+        {
+            playerMovement.Crouch();
+
+            //TODO: Remover quando adicionarmos animacao
+            spriteRenderer.sprite = crouchedSprite;
+            //
+        }
+        else if (playerInput.IsCrouchButtonUp())
+        {
+            playerMovement.UnCrouch();
+
+            //TODO: Remover quando adicionarmos animacao
+            spriteRenderer.sprite = idleSprite;
+            //
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 GetMovementInput()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        return new Vector2(horizontalInput, 0);
+    }
+
+    public bool IsJumpButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    public bool IsJumpButtonHeld()
+    {
+        return Input.GetKey(KeyCode.Space);
+    }
+
+    public bool IsCrouchButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.S);
+    }
+
+    public bool IsCrouchButtonUp()
+    {
+        return Input.GetKeyUp(KeyCode.S);
+    }
+}
