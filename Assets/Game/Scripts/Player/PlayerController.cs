0c51b6ec93425a9dc0266154600e60d105e619c0
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-02-06 16:37:58 -0700

    Aula 08 - Final

--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -16,11 +16,14 @@ public class PlayerController : MonoBehaviour
     public Sprite idleSprite;
 
     [Header("Camera")]
+
     public Transform cameraTarget;
-    [Range(0, 5)]
-    public float cameraTargetOffsetX;
-    [Range(0.5f, 5.0f)]
-    public float cameraTargetFlipSpeed;
+    [Range(0.0f, 5.0f)]
+    public float cameraTargetOffsetX = 2.0f;
+    [Range(0.5f, 50.0f)]
+    public float cameraTargetFlipSpeed = 2.0f;
+    [Range(0.0f, 5.0f)]
+    public float characterSpeedInfluence = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -45,18 +48,6 @@ public class PlayerController : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
-
-        bool isFacingRight = spriteRenderer.flipX == false;
-        float offsetTargetX = isFacingRight ? cameraTargetOffsetX : -cameraTargetOffsetX;
-        Vector3 cameraTargetLocalPosition = new Vector3(
-            offsetTargetX, 
-            cameraTarget.localPosition.y, 
-            cameraTarget.localPosition.z);
-
-        cameraTarget.localPosition = Vector3.Lerp(
-            cameraTarget.localPosition, 
-            cameraTargetLocalPosition, 
-            Time.deltaTime * cameraTargetFlipSpeed);
 
         //Pulo
         if (playerInput.IsJumpButtonDown())
@@ -85,5 +76,19 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.sprite = idleSprite;
             //
         }
+    }
+
+    private void FixedUpdate()
+    {
+        // Controle do target da camera dependendo da direcao do sprite e da velocidade do jogador
+        bool isFacingRight = spriteRenderer.flipX == false;
+        float targetOffsetX = isFacingRight ? cameraTargetOffsetX : -cameraTargetOffsetX;
+
+        float currentOffsetX = Mathf.Lerp(cameraTarget.localPosition.x, targetOffsetX, Time.fixedDeltaTime * cameraTargetFlipSpeed);
+
+        currentOffsetX += playerMovement.CurrentVelocity.x * Time.fixedDeltaTime * characterSpeedInfluence;
+
+        cameraTarget.localPosition = new Vector3(currentOffsetX, cameraTarget.localPosition.y, cameraTarget.localPosition.z);
+        //
     }
 }
